70797da34864d7d82f62d7309313c7c8da7220ee
unknown <lovinmathew@hotmail.com>
2020-01-19 01:57:32 -0600

    Changed

--- script.js
+++ script.js
@@ -9,11 +9,18 @@ const timerElement = document.getElementById('timer');
 const mainBody = document.getElementById('main');
 const enterName = document.getElementsByClassName('form-group');
 const nameButton = document.getElementById('name-btn');
+const clearButton = document.getElementById('clear-btn');
+const name = document.getElementById('name');
+
 
 let shuffledQuestion, currentQuestionIndex, flag, qNumber; 
 
 startButton.addEventListener('click', startQuiz);
-var scoreCount, secondsLeft, score, timer;
+nameButton.addEventListener('click', displayHighscores);
+name.addEventListener('keyup', () => {console.log(name.value)});
+clearButton.addEventListener('click', clearHighscores);
+
+var scoreCount, secondsLeft, score=0, timer, highScore;
 
 function startQuiz(){
  scoreCount=0;
@@ -21,6 +28,9 @@ function startQuiz(){
 //  console.log('here');
  description.classList.add('hide'); // remove description
  startButton.classList.add('hide'); //remove start button
+ enterName[0].classList.add('hide');
+ nameButton.classList.add('hide');
+ clearButton.classList.add('hide');
  shuffledQuestion = questions.sort(() => Math.random() - .5);
  currentQuestionIndex=0;
  answersElement.classList.remove('hide');//shows the answer choice
@@ -35,6 +45,10 @@ function setTime() {
   
       if(secondsLeft === 0 || (shuffledQuestion.length < currentQuestionIndex +1)) {   // resetting the time if the time reaches 0 or the question list is completed. 
         clearInterval(timerInterval);
+      }
+
+      if(secondsLeft === 0){
+        resetFormat();
       }
   
     }, 1000);
@@ -138,15 +152,53 @@ function resetFormat(){
     nameButton.classList.remove('hide');
     timer=secondsLeft;                                                //capture timer data
     description.innerText = 'Your score is: '+score;
-    
     console.log(timer);
     console.log(score);
     console.log(timer+score);                                         //capture high score
-    //displayHighscores();
+    highScore = timer+score;
+    
+    
+    //displayHighscores(highScore);
     resetState();
 }
 
-// function displayHighscores(){
 
 
-// }
+// const mostRecentScore = localStorage.getItem('mostRecentScore');
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+console.log(highScores);
+// console.log(mostRecentScore);
+function displayHighscores(){
+    resetState();
+    console.log('WIN !');
+    questionElement.innerText = 'Highscores:';
+    description.classList.add('hide');
+    enterName[0].classList.add('hide');
+    nameButton.classList.add('hide');
+    clearButton.classList.remove('hide');
+
+    const scores={
+        score: highScore,
+        name: name.value
+    };
+    console.log(scores);
+    highScores.push(scores);
+    console.log(highScores);
+    highScores.sort((a,b) => b.score-a.score);
+    highScores.splice(5);
+    localStorage.setItem('highScores',JSON.stringify(highScores));
+    // for (var player=0; player<highScores.length; player++){
+    var newScoreList = document.createElement('h2');
+    var textnode = document.createTextNode("Water");  
+    newScoreList.append(textnode);
+    mainBody.insertBefore(newScoreList,mainBody.childNodes[2]);
+    // }
+}
+
+function clearHighscores(){
+    var choose = confirm('Clear highscores ?');
+    if(choose){
+    highScores.length=0;
+    alert('Highscores cleared !');
+    }
+}
